2606424a9148f92d5f0fd87aa02a9a8b0a9bca70
Florian Sihler <florian.sihler@uni-ulm.de>
2022-07-01 15:59:58 +0200

    prepping for js and generall refinement

--- example object and js/data.sh
+++ example object and js/data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # only zsh can use the ';' trick... bash can't i think
-make run& ; sleep 0.5; ps -o pid,ppid,cpuid,cmd > run.data
+make run& ; sleep 6; ps -o pid,ppid,cpuid,cmd > run.data
